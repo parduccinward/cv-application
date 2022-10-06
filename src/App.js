4465c4739beebbb75b056656aa25e8d0e4a30984
Diego <dpardoblacutt@gmail.com>
2022-10-06 15:38:46 -0400

    Change App from functional component to class component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div>Hello World!</div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
 }
 
-export default App;
+export default App
