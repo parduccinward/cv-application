58f0019794e39dbff7ef0b2cff49674fa35e6bb7
Diego <dpardoblacutt@gmail.com>
2022-10-07 19:28:02 -0400

    Change style for section buttons

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import "../styles/Section.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Section = (props) => {
     const id = props.id;
     const title = props.title;
+    const icon = props.icon;
   return (
-    <div id={id}className="flip">
-        <a href="/#">
-            <div className="front">{title}</div>
-            <div className="back">{title}</div>
-        </a>
+    <div id={id}className="section">
+      <FontAwesomeIcon icon={icon} />
+      <p>{title}</p>
     </div>
   )
 }
--- src/components/SectionContainer.js
+++ src/components/SectionContainer.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Section from "./Section";
 import "../styles/SectionContainer.css";
+import { fa1, fa2, fa3 } from '@fortawesome/free-solid-svg-icons'
 
 const SectionContainer = () => {
   return (
     <div className="section-container">
-        <Section title="1"id="personal-info"/>
-        <Section title="2"id="educational-experience"/>
-        <Section title="3"id="practical-experience"/>
+        <Section title="Personal information"id="personal-info" icon={fa1}/>
+        <Section title="Educational experience"id="educational-experience" icon={fa2}/>
+        <Section title="Practical experience"id="practical-experience" icon={fa3}/>
     </div>
   )
 }
